6ee617971c09c5979a078d32c5027f972f843a5e
oriest <oriestdjelloh@gmail.com>
2021-10-10 09:44:47 +0000

    mise en place

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header/>
+    <Stock/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Stock from '@/components/Stock.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Header,
+    Stock
   }
 }
 </script>
